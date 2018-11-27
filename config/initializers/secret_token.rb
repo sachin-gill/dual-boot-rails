ad4c0ef905b80ac6695bb73538df08adf8fd8d4f
Sachin <sachin@sachingill.compro>
2018-11-27 16:19:49 +0530

    dual_boot: Adding rails 3.2.2 sample app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DualBootRails::Application.config.secret_token = '3ab90d85a44faa00a89084fe9af78b3cec5489132b3b189384bfaa0bec8a1e52aad9576d137b86cdc5c7482bb0462a61b4739fc3c2acc5dab919c9762de0c589'
